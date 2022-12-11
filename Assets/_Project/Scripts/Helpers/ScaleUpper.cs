19534ce04a16fd3df35aa16aac83b84d0819c1a7
safa <sdedeakay@yahoo.com>
2022-12-11 23:26:19 +0300

    feat: Upgrade UI

---
+++ Assets/_Project/Scripts/Helpers/ScaleUpper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Helpers
+{
+    public class ScaleUpper : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            transform.ScaleUp(Vector3.one, .3f);
+        }
+    }
+}
